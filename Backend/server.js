4d80652dc0c795fa26c4f330e507a5a8e8235c56
Groot-2001 <shivasilmawala841@gmail.com>
2022-07-11 16:31:10 +0530

    login and register ui created

--- Backend/server.js
+++ Backend/server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const {chats} = require("./data/data");
 const dotenv = require("dotenv");
 const app = express();
+const connectDB = require("./config/db");
 
 dotenv.config();
+connectDB();
 
 const port = process.env.PORT;
 
